dcb1a3b0b04eafa31dc399f5586e12e06c4ddd11
Deepika DT <deepika@digitaltriumph.co.in>
2025-03-23 15:18:27 +0530

    all spices changed

--- src/components/Spices/Coriander.tsx
+++ src/components/Spices/Coriander.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Table, Button } from 'react-bootstrap';
-import rice1 from '../../assets/images/spices/Coriander-banner.jpg'
+import rice1 from '../../assets/images/spices/coriander.jpg'
 import Header from '../Header';
 import Footer from '../Footer';
 import { useNavigate } from 'react-router-dom';
--- src/components/Spices/StarAnise.tsx
+++ src/components/Spices/StarAnise.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Table, Button } from 'react-bootstrap';
-import rice1 from '../../assets/images/spices/staranise-banner.jpg'
+import rice1 from '../../assets/images/spices/star.jpg'
 import Header from '../Header';
 import Footer from '../Footer';
 import { useNavigate } from 'react-router-dom';
